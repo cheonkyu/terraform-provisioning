d50f28d587537424c383a635facb93378644a406
cheonkyu0 <cheonkyu.dev@gmail.com>
2025-02-19 11:34:12 +0900

    feat: ecs

--- terraform/ecs/demo/locals.tf
+++ terraform/ecs/demo/locals.tf
@@ -1,7 +1,7 @@
 locals {
   name = "demo-tmcdapne2"
 
-  container_name = "demo"
+  container_name = "demo1"
   container_port = 8080
   image          = "066346343248.dkr.ecr.ap-northeast-2.amazonaws.com/demoapp:d2edeae84"
   tags = {
