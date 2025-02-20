703358d76e890cbe7142d2f4f5f5bb6a5e024327
cheonkyu0 <cheonkyu.dev@gmail.com>
2025-02-20 17:28:26 +0900

    feat: 시나리오 1-tier-ec2 테라폼 개발

---
+++ terraform/scenario/00.1-tier-ec2/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    region  = "ap-northeast-2"
+    bucket  = "cheonkyu-atlantics-tfstate"
+    key     = "provising/terraform/scenario/00.server/terraform.tfstate"
+    profile = "default"
+  }
+}
---
+++ terraform/scenario/00.1-tier-ec2/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default     = "ap-northeast-2"
+  description = "AWS 리전"
+}
+
+variable "name" {
+  default     = "1-tier-ec2"
+  description = "AWS 리전"
+}
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR"
+}
+
+variable "environment" {
+  default     = "dev"
+  description = "dev/stg/prod"
+  validation {
+    condition     = contains(["dev", "stg", "prod"], var.environment)
+    error_message = "dev/stg/prod 환경이 아님"
+  }
+}
